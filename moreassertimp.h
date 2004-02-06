afee15476b47fb430e3f325db6c2199db8f78c6e
zooko <zooko>
2004-02-06 16:47:22 +0000

    stupid CVS

--- moreassertimp.h
+++
@@ -1,8 +0,0 @@
-#include "stdio.h"
-
-/* An assertion failure message will look like this: */
-/* test.c:238: test_minmax_strict: Assertion `1==0' failed. */
-int _runtime_assert_fail(char const* filename, int lineno, char const* funcname, char const* expr, char const* msg);
-
-#define _runtime_assert(expr, msg) ((void)((expr) ? 0 : _runtime_assert_fail(__FILE__, __LINE__,  __func__, #expr, msg)))
-
